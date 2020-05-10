32e0445830514fb91f4d3833f0fa30cffcde8d97
iMine141 <846945674@qq.com>
2020-05-10 16:45:33 +0800

    first

--- goods/src/main/java/com/mlj/it/goods/GoodsApplication.java
+++ goods/src/main/java/com/mlj/it/goods/GoodsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoodsApplication {
-
+    //1
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class, args);
     }
